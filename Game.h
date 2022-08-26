de908ddd6adf72fea074ae0fd506296a46b0d02f
m@rtin.bz <m@rtin.bz>
2022-08-26 20:37:35 -0200

    Initial commit

---
+++ Game.h
@@ -0,0 +1,56 @@
+//
+// Created by martin on 26-08-22.
+//
+
+#pragma once
+
+#include "SDL2/SDL.h"
+
+struct Vector2
+    {
+        float x;
+        float y;
+    };
+
+class Game
+   {
+   public:
+      Game ();
+
+      // Initialize the game
+      bool Initialize ();
+
+      // Runs the game loop until the game is over
+      void RunLoop ();
+
+      // Shutdown the game
+      void Shutdown ();
+
+   private:
+
+      // Helper functions for the game loop
+      void ProcessInput ();
+
+      void UpdateGame ();
+
+      void GenerateOutput ();
+
+      // Window created by SDL
+      SDL_Window *mWindow;
+
+      // Game should continue to run
+      bool mIsRunning;
+
+      SDL_Renderer *mRenderer;
+
+      Vector2 mPaddlePos;
+
+      Vector2 mBallPos;
+
+      Uint32 mTicksCount;
+
+      int mPaddleDir;
+
+      Vector2 mBallVel;
+
+   };
