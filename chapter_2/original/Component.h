89251d4eff710cc9ca9c2f5257bc9225792d1391
m@rtin.bz <m@rtin.bz>
2022-08-27 20:53:07 -0200

    Chapter 2 original & Assets

---
+++ chapter_2/original/Component.h
@@ -0,0 +1,30 @@
+//
+// Created by martin on 27-08-22.
+//
+#pragma once
+
+#pragma once
+
+class Component
+   {
+   public:
+      // Constructor
+      // (the lower the update order, the earlier the component updates)
+      Component (class Actor *owner, int updateOrder = 100);
+
+      // Destructor
+      virtual ~Component ();
+
+      // Update this component by delta time
+      virtual void Update (float deltaTime);
+
+      int GetUpdateOrder () const
+        { return mUpdateOrder; }
+
+   protected:
+      // Owning actor
+      class Actor *mOwner;
+
+      // Update order of component
+      int mUpdateOrder;
+   };
